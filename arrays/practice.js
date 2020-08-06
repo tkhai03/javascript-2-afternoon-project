24619b4142afd7b4ea3ac6059fb7cf9726e6a2a5
Thang Khai <tnkhai94@gmail.com>
2020-08-06 16:13:01 -0600

    Initial commit

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first (arr) {
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -34,6 +36,9 @@ var arr = [40,50,60];
 
 //Code Here
 
+function last (arr) {
+  return arr.slice(-1)[0]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,6 +53,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family) {
+  for (i = 0; i <= family.length - 1; i++){
+   alert(family[i]);
+  }
+}
+
 
 
 
@@ -63,6 +74,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+
+function reversedLooper(letters){
+  for (i = letters.length - 1; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
+
 
 
 
@@ -80,6 +98,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Code Here
 
 
+
+function evenFinder(nums){
+  let arr = [];
+  for(i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0 ){
+      arr.push(nums[i]);
+    }
+  }
+  return arr;
+}
 
 
 
